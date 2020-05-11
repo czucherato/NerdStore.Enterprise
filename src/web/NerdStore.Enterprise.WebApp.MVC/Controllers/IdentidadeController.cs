d4bb91cbfe80a0f9418796411e6d2320438e729c
Carlos Zucherato <carlos.zucheratto@gmail.com>
2020-05-11 14:29:39 -0300

    Models, Views e Controllers de login

--- src/web/NerdStore.Enterprise.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NerdStore.Enterprise.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Enterprise.WebApp.MVC.Models;
 
 namespace NerdStore.Enterprise.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistroViewModel parametros)
+        {
+            if (!ModelState.IsValid) return View(parametros);
+            //API - Registro
+            if (false) return View(parametros);
+
+            //Realizar login na APP
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLoginViewModel parametros)
+        {
+            if (!ModelState.IsValid) return View(parametros);
+            //API - Login
+            if (false) return View(parametros);
+
+            //Realizar login na APP
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> LogOut()
+        {
+            return RedirectToAction("Login", "Identidade");
+        }
     }
 }
