53e31bb9ca0589fbdd5d278558814789a885555f
Carlos Zucherato <carlos.zucheratto@gmail.com>
2020-05-14 17:42:12 -0300

    Modelagem API Catalogo

---
+++ src/services/NerdStore.Enterprise.Catalogo.API/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NerdStore.Enterprise.Core.Domain_Objects;
+
+namespace NerdStore.Enterprise.Catalogo.API.Models
+{
+    public class Produto : Entity
+    {
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public string Imagem { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+    }
+}
