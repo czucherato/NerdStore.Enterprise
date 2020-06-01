1f6728e99a8b2a15713ce287593b952812adecc4
Carlos Zucherato <carlos.zucheratto@gmail.com>
2020-06-01 20:04:47 -0300

    Entendendo os Delegating Handles

--- src/web/NerdStore.Enterprise.WebApp.MVC/Configurations/DependencyInjectionConfig.cs
+++ src/web/NerdStore.Enterprise.WebApp.MVC/Configurations/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Enterprise.WebApp.MVC.Services;
 using NerdStore.Enterprise.WebApp.MVC.Extensions;
+using NerdStore.Enterprise.WebApp.MVC.Services.Handlers;
 
 namespace NerdStore.Enterprise.WebApp.MVC.Configurations
 {
@@ -9,8 +10,9 @@ namespace NerdStore.Enterprise.WebApp.MVC.Configurations
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>().AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
         }
---
+++ src/web/NerdStore.Enterprise.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using NerdStore.Enterprise.WebApp.MVC.Extensions;
+
+namespace NerdStore.Enterprise.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        private readonly IUser _user;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string> { authorizationHeader });
+            }
+
+            var token = _user.ObterUserToken();
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
