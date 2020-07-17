df10163cae1da15cdb950ec0b5bc73c25fa47844
carlos.zucheratto <carlos.zucheratto@gmail.com>
2020-07-17 17:50:04 -0300

    Configuração da API de Pagamentos

---
+++ src/services/NerdStore.Enterprise.Pagamento.API/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using NerdStore.Enterprise.Pagamento.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Enterprise.WebAPI.Core.Usuario;
+using NerdStore.Enterprise.Pagamento.API.Models;
+using NerdStore.Enterprise.Pagamento.API.Data.Repository;
+
+namespace NerdStore.Enterprise.Pagamento.API.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // API
+            services.AddScoped<IAspNetUser, AspNetUser>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Data
+            services.AddScoped<PagamentoContext>();
+            services.AddScoped<IPagamentoRepository, PagamentoRepository>();
+        }
+    }
+}
---
+++ src/services/NerdStore.Enterprise.Pagamento.API/Configurations/MessageBusConfig.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.Enterprise.Core.Utils;
+using NerdStore.Enterprise.MessageBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NerdStore.Enterprise.Pagamento.API.Configurations
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+        }
+    }
+}
